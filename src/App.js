9e806f1dc86993905bcb761a0a0b85a695aba156
ArbindKrPrasad <arbind.kr25801@gmail.com>
2021-04-08 16:57:55 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import './App.css';
 import React, { Component } from 'react';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome'
+import Message from './components/message'
 import Hello from './components/Hello'
+import Counter from './components/Counter';
+import ParentComponent from './components/ParentComponent';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet name='Bruce' heroName = 'Batman'>
+        <FunctionClick />
+        <ClassClick />
+        {/* <ParentComponent /> */}
+        {/* <Counter/> */}
+        {/* <Message /> */}
+        {/* <Greet name='Bruce' heroName = 'Batman'>
           <p>This  is a children tag</p>
         </Greet>
         <Greet name='Clark' heroName = 'Soider Man'/>
-        <Greet name='Bruce' heroName = 'Wonder woman'/>
+        <Greet name='Bruce' heroName = 'Wonder woman'/> */}
       </div>
     );
   }
